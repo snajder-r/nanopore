647f3ffd432771110ea69dff4735de2c706f1270
Rene Snajder <snajder@stud.uni-heidelberg.de>
2020-08-29 19:00:05 +0200

    Updated nanopore rules to match recent changes in folder structure

---
+++ do_snakemake.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cluster_script="bsub {params.misc} -n {params.slots} -W {params.runtime} -M {params.memusage}  -R \"rusage[mem={params.memusage}]\" -o {basedir}/logs/{params.jobname}.log -e {basedir}/logs/{params.jobname}.err"
+
+snakemake --cluster "$cluster_script" --jobs 64 --latency-wait 120 $@
+
--- do_snakmake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-cluster_script="bsub {params.misc} -n {params.slots} -W {params.runtime} -M {params.memusage}  -R \"rusage[mem={params.memusage}]\" -o ../logs/{params.jobname}.log -e ../logs/{params.jobname}.err"
-
-snakemake --cluster "$cluster_script" --jobs 64 --latency-wait 120 $@
-
